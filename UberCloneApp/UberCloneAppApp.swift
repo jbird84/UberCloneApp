3a4b359fd94edc00e14f118accf46436592907c0
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-01-24 11:16:10 -0600

    Initial Commit

---
+++ UberCloneApp/UberCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneAppApp.swift
+//  UberCloneApp
+//
+//  Created by Kinney Kare on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
